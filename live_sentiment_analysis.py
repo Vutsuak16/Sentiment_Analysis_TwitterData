f8f31e50b52d09768def420751374723653a64a1
kaustuv deolal <vutsuak96@gmail.com>
2016-05-17 13:50:39 +0530

    StdOutListener class created to manage the streams

--- live_sentiment_analysis.py
+++ live_sentiment_analysis.py
@@ -1,33 +1,30 @@
 __name__ = "vutsuak"
 
 import tweepy
+import json
 
 ACCESS_TOKEN = '2309050262-YxLP1tfqWQrWc1blVyH8jvuxbJ2fPZCPwb0L4Sl'
 ACCESS_SECRET = 'eK2IBq7PqyI2iisqdw5ouppkJJCE02OSXG0AQpDog0uZR'
 CONSUMER_KEY = 'McCLzJjS7kc1aD0dCYLsUMTme'
 CONSUMER_SECRET = '7suZUiu1OUIo1TUpiiyHPDLNo8fWMkjf7d6N9zBCLKaX3JLwjb'
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 
-api = tweepy.API(auth)
-'''following = []
-keyword = raw_input("enter a keyword")  # takes only one keyword for the  time being
-keyword = "#" + keyword
-for user in tweepy.Cursor(api.friends, screen_name="kaustuv deolal").items():
-    following.append((str(user.screen_name)))
+class StdOutListener(tweepy.StreamListener):
+    def on_data(self, data):
+        j= json.loads(data)
+        print j['text']
+        return True
+
+    def on_error(self, status):
+        print status
 
 
-ct = 5 #only for 5 tweets
-for tweet in tweepy.Cursor(api.search, q=keyword).items():
-    if ct == 0:
-        break
-    try:
-        if tweet.user.screen_name not in following:
-                api.create_friendship(tweet.user.id)
-                api.create_favorite(tweet.id)
-    except:
-        pass
+if __name__ == 'vutsuak':
+    # This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+    stream = tweepy.Stream(auth, l)
 
-    ct -= 1
-'''
+    # This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track=['bangalore'])
