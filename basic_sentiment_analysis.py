a8cbd37db660b844aa2b837ab4cd9941cb8713ab
kaustuv deolal <vutsuak96@gmail.com>
2016-05-27 14:02:27 +0530

    sentiment analysis started

---
+++ basic_sentiment_analysis.py
@@ -0,0 +1,13 @@
+pos_tweets = [('I love this car', 'positive'),
+              ('This view is amazing', 'positive'),
+              ('I feel great this morning', 'positive'),
+              ('I am so excited about the concert', 'positive'),
+              ('He is my best friend', 'positive')]
+
+neg_tweets = [('I do not like this car', 'negative'),
+              ('This view is horrible', 'negative'),
+              ('I feel tired this morning', 'negative'),
+              ('I am not looking forward to the concert', 'negative'),
+              ('He is my enemy', 'negative')]
+
+print pos_tweets+neg_tweets
